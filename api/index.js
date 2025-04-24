9260cfdbb56b2ebdedb4c653a6c15c02c738499d
oiat11 <fu.qia@northeastern.edu>
2025-04-24 11:25:22 -0700

    update cors

--- api/index.js
+++ api/index.js
@@ -15,24 +15,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = process.env.NODE_ENV === "production" 
-  ? ["https://five500-project-03-1.onrender.com"] 
-  : ["http://localhost:5173"];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // 允许没有 origin 的请求 (比如 curl 命令或某些移动端请求)
-    if (!origin) return callback(null, true);
-    // 检查请求的 origin 是否在允许列表里
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = '跨域策略不允许来自此源的访问。'; // The CORS policy for this site does not allow access from the specified Origin.
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: process.env.NODE_ENV === "production" 
+    ? "https://five500-project-03-1.onrender.com"
+    : "http://localhost:5173",
   methods: "GET, POST, PUT, DELETE, OPTIONS",
-  credentials: true, // 如果需要发送 cookie
+  credentials: true,
 }));
+
+app.options('*', cors());
+
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => {
